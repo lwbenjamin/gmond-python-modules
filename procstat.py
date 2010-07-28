b52158f59aeffe864098620c35fd33068e61cdb1
pdt256 <pdt256@45b1daad-ef76-c2da-c1da-ee31385fce6c>
2010-07-28 19:47:10 +0000

    Updated to vertion 1.1.0

--- procstat.py
+++ procstat.py
@@ -2,56 +2,96 @@
 ###
 ###  Notes:
 ###    This script exposes values for CPU and memory utilization
-###    using the Linux command "ps" for user defined processes. You
-###    can monitor single or multiple processes groups. This comes
-###    in handy for monitoring services like Apache MPM prefork which
-###    launches multiple child processes. Keep in mind that a well
-###    formed regular expression is suggested to be used to limit the
-###    possibility of a conflict from another service or command line
-###    process.
+###    for running processes. You can retrieve the process ID from
+###    either providing a pidfile or an awk regular expression.
+###    Using a pidfile is the most efficient and direct method.
+###
+###    When using a regular expression, keep in mind that there is
+###    a chance for a false positive. This script will help to avoid
+###    these by only returning parent processes. This means that
+###    the results are limited to processes where ppid = 1.
 ###
 ###    This script also comes with the ability to test your regular
-###    expressions via command line arguments.
+###    expressions via command line arguments "-t".
 ###
 ###  Testing:
-###    The following example is a good regular expression
-###    to use to monitor Apache. It is a very specific and
-###    direct expression: \/usr\/sbin\/httpd
+###   -- This is a correct examples of how to monitor apache.
 ###
-###    $ python procstat.py -p httpd -r '\/usr\/sbin\/httpd' -t
-###    Testing httpd: /\/usr\/sbin\/httpd/
-###    CPU, MEM, PID, USER, ARGS
-###     0.2  7956 17642 root     /usr/sbin/httpd
-###     0.0  9380 17644 apache   /usr/sbin/httpd
-###     0.2  9420 17645 apache   /usr/sbin/httpd
-###     0.1  9404 17646 apache   /usr/sbin/httpd
-###     0.3  9384 17647 apache   /usr/sbin/httpd
-###     0.0  9400 17648 apache   /usr/sbin/httpd
-###    Totals:
-###    0.8 54944
+###      $ python procstat.py -p httpd -v '/var/run/httpd.pid' -t
+###       Testing httpd: /var/run/httpd.pid
+###       Processes in this group:
+###       PID, ARGS
+###       11058 /usr/sbin/httpd
+###       8817 /usr/sbin/httpd
+###       9000 /usr/sbin/httpd
+###       9001 /usr/sbin/httpd
 ###
-###  Command Line:
-###    $ python procstat.py -p httpd,opennms,splunk \
-###    -r '\/usr\/sbin\/httpd','java.*Dopennms','splunkd.*start|twistd.*SplunkWeb'
+###       waiting 2 seconds
+###       procstat_httpd_mem: 202076 KB [The total memory utilization]
+###       procstat_httpd_cpu: 0.3 percent [The total percent CPU utilization]
 ###
-###     procstat_httpd_mem: 194744 KB [The total memory utilization]
-###     procstat_splunk_mem: 546912 KB [The total memory utilization]
-###     procstat_opennms_mem: 608724 KB [The total memory utilization]
-###     procstat_httpd_cpu: 0.0 percent [The total percent CPU utilization]
-###     procstat_splunk_cpu: 0.9 percent [The total percent CPU utilization]
+###   -- This example shows a regex that returns no processes with a
+###      ppid of 1.
+###
+###      $ python procstat.py -p test -v 'wrong' -t
+###       Testing test: wrong
+###       failed getting pgid: no process returned
+###      ps -Ao pid,ppid,pgid,args | awk 'wrong && $2 == 1 && !/awk/ && !/procstat\.py/ {print $0}'
+###
+###   -- This example shows a regex that returns more than one process
+###      with a ppid of 1.
+###
+###      $ python procstat.py -p test -v '/mingetty/' -t
+###       Testing test: /mingetty/
+###       failed getting pgid: more than 1 result returned
+###      ps -Ao pid,ppid,pgid,args | awk '/mingetty/ && $2 == 1 && !/awk/ && !/procstat\.py/ {print $0}'
+###       7313     1  7313 /sbin/mingetty tty1
+###       7314     1  7314 /sbin/mingetty tty2
+###       7315     1  7315 /sbin/mingetty tty3
+###       7316     1  7316 /sbin/mingetty tty4
+###       7317     1  7317 /sbin/mingetty tty5
+###       7318     1  7318 /sbin/mingetty tty6
+###
+###  Command Line Example:
+###    $ python procstat.py -p httpd,opennms,splunk,splunk-web \
+###    -v '/var/run/httpd.pid','/opt/opennms/logs/daemon/opennms.pid','/splunkd.*start/','/twistd.*SplunkWeb/'
+###
+###     procstat_httpd_mem: 202068 KB [The total memory utilization]
+###     procstat_splunk_mem: 497848 KB [The total memory utilization]
+###     procstat_splunk-web_mem: 32636 KB [The total memory utilization]
+###     procstat_opennms_mem: 623112 KB [The total memory utilization]
+###     procstat_httpd_cpu: 0.3 percent [The total percent CPU utilization]
+###     procstat_splunk_cpu: 0.6 percent [The total percent CPU utilization]
+###     procstat_splunk-web_cpu: 0.1 percent [The total percent CPU utilization]
 ###     procstat_opennms_cpu: 7.1 percent [The total percent CPU utilization]
 ###
-###  Example Regex:
-###    httpd:   \/usr\/sbin\/httpd
-###    mysqld:  \/usr\/libexec\/mysqld
-###    splunk:  splunkd.*start|twistd.*SplunkWeb
-###    opennms: java.*Dopennms
-###    netflow: java.*NetFlow
+###  Example Values:
+###    httpd:      /var/run/httpd.pid or \/usr\/sbin\/httpd
+###    mysqld:     /var/run/mysqld/mysqld.pid or /\/usr\/bin\/mysqld_safe/
+###    postgresql: /var/run/postmaster.[port].pid or /\/usr\/bin\/postmaster.*[port]/
+###    splunk:     /splunkd.*start/
+###    splunk-web: /twistd.*SplunkWeb/
+###    opennms:    /opt/opennms/logs/daemon/opennms.pid or java.*Dopennms
+###    netflow:    /java.*NetFlow/
+###    postfix:    /var/spool/postfix/pid/master.pid or /\/usr\/libexec\/postfix\/master/
 ###
+###  Error Tests:
+###    python procstat.py -p test-more,test-none,test-pidfail -v '/java/','/javaw/','java.pid' -t
 ###
 ###  Changelog:
 ###    v1.0.1 - 2010-07-23
-###       * Initial version
+###      * Initial version
+###
+###    v1.1.0 - 2010-07-28
+###      * Modified the process regex search to find the parent
+###        process and then find all processes with the same process
+###        group ID (pgid). "ps" is only used for regex searching on
+###        the initial lookup for the parent pid (ppid). Now all
+###        subsuquent calls use /proc/[pid]/stat for CPU jiffies, and
+###        /proc/[pid]/statm for memory rss.
+###      * Added testing switch "-t" to help troubleshoot a regex
+###      * Added display switches "-s" and "-m" to format the outup
+###        of /proc/[pid]/stat and /proc/[pid]/statm
 ###
 
 ###  Copyright Jamie Isaacs. 2010
@@ -61,41 +101,162 @@
 import time
 import subprocess
 import traceback, sys
+import os.path
+import glob
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s\t Thread-%(thread)d - %(message)s", filename='/tmp/procstat_log')
+logging.basicConfig(level=logging.ERROR, format="%(asctime)s - %(name)s - %(levelname)s\t Thread-%(thread)d - %(message)s", filename='/tmp/gmond.log', filemode='w')
 logging.debug('starting up')
 
 last_update = 0
 stats = {}
+last_val = {}
+pgid_list = {}
 
 MAX_UPDATE_TIME = 15
 
+# clock ticks per second... jiffies (HZ)
+JIFFIES_PER_SEC = os.sysconf('SC_CLK_TCK')
+
+# KiB
+PAGE_SIZE=os.sysconf('SC_PAGE_SIZE') / 1024
+
 PROCESSES = {}
 
-def test(PROCESSES):
-	logging.debug('testing processes and regex: ' + str(PROCESSES))
-	
-	for proc,regex in PROCESSES.items():
-		print('Testing ' + proc + ': /' + regex + '/')
-		print('CPU, MEM, PID, USER, ARGS')	
+def readCpu(pid):
+	try:
+		stat = file('/proc/' + pid + '/stat', 'rt').readline().split()
+		#logging.debug(' stat (' + pid + '): ' + str(stat))
+		utime = int(stat[13])
+		stime = int(stat[14])
+		cutime = int(stat[15])
+		cstime = int(stat[16])
+		return (utime + stime + cutime + cstime)
+	except:
+		logging.warning('failed to get (' + str(pid) + ') stats')
+		return 0
 
-		awk_cmd = "ps -Ao pcpu,rss,pid,user,args | awk '/" + regex + "/ && !/awk/ && !/procstat\.py/ {_cpu+=$1; _rss+=$2; print $0} END {print \"Totals:\"; printf(\"%.1f %d\", _cpu,_rss)}'"
-		p = subprocess.Popen(awk_cmd, shell=True, stdout=subprocess.PIPE)
+def get_pgid(proc):
+	logging.debug('getting pgid for process: ' + proc)
+	ERROR = 0
+
+	if pgid_list.has_key(proc) and os.path.exists('/proc/' + pgid_list[proc][0]):
+		return pgid_list[proc]
+
+	val = PROCESSES[proc]
+	# Is this a pidfile? Last 4 chars are .pid
+	if '.pid' in val[-4:]:
+		if os.path.exists(val):
+			logging.debug(' pidfile found')
+			ppid = file(val, 'rt').readline().strip()
+			pgid = file('/proc/' + ppid + '/stat', 'rt').readline().split()[4]
+		else:
+			raise Exception('pidfile (' + val + ') does not exist')
+
+	else:
+		# This is a regex, lets search for it
+		regex = PROCESSES[proc]
+		cmd = "ps -Ao pid,ppid,pgid,args | awk '" + regex + " && $2 == 1 && !/awk/ && !/procstat\.py/ {print $0}'"
+		p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		out, err = p.communicate()
 
 		if p.returncode:
-			print('failed getting stats')
+			raise Exception('failed executing ps\n' + cmd + '\n' + err)
+
+		result = out.strip().split('\n')
+		logging.debug(' result: ' + str(result))
+
+		if len(result) > 1:
+			raise Exception('more than 1 result returned\n' + cmd + '\n' + out.strip())
+
+		if result[0] in '':
+			raise Exception('no process returned\n' + cmd)
+
+		res = result[0].split()
+		ppid = res[0]
+		pgid = res[2]
+
+	if os.path.exists('/proc/' + ppid):
+		logging.debug(' ppid: ' + ppid + ' pgid: ' + pgid)
+		return (ppid, pgid)
+	else:
+		return ERROR
+
+def get_pgroup(ppid, pgid):
+	'''Return a list of pids having the same pgid, with the first in the list being the parent pid.'''
+	logging.debug('getting pids for ppid/pgid: ' + ppid + '/' + pgid)
+
+	try:
+		# Get all processes in this group
+		p_list = []
+		for stat_file in glob.glob('/proc/[1-9]*/stat'):
+			stat = file(stat_file, 'rt').readline().split()
+			if stat[4] == pgid:
+				p_list.append(stat[0])
+
+		# place pid at the top of the list
+		p_list.remove(ppid)
+		p_list.insert(0, ppid)
+
+		logging.debug('p_list: ' + str(p_list))
+
+		return p_list
+
+	except:
+		logging.warning('failed getting pids')
+
+def get_rss(pids):
+	logging.debug('getting rss for pids')
+
+	rss = 0
+	for p in pids:
+		try:
+			statm = open('/proc/' + p + '/statm', 'rt').readline().split()
+			#logging.debug(' statm (' + p + '): ' + str(statm))
+		except:
+			# Process finished, ignore this mem usage
+			logging.warning(' failed getting statm for pid: ' + p)
 			continue
 
-		print(out)
+		rss += int(statm[1])
+
+	# Convert to KiB
+	rss *= PAGE_SIZE
+	return rss
+
+def test(params):
+	global PROCESSES, MAX_UPDATE_TIME
+
+	MAX_UPDATE_TIME = 2
+
+	logging.debug('testing processes: ' + str(params))
+
+	PROCESSES = params
+
+	for proc,val in PROCESSES.items():
 		print('')
+		print(' Testing ' + proc + ': ' + val) 
+
+		try:
+			(ppid, pgid) = get_pgid(proc)
+		except Exception, e:
+			print(' failed getting pgid: ' + str(e))
+			continue
+
+		pids = get_pgroup(ppid, pgid)
+
+		print(' Processes in this group: ')
+		print(' PID, ARGS')
+		for pid in pids:
+			# Read from binary file containing command line arguments
+			args = file('/proc/' + pid + '/cmdline', 'rt').readline().replace('\0', ' ')
+			print(' ' + pid + ' ' + args)
 
 	logging.debug('success testing')
 
 def update_stats():
 	logging.debug('updating stats')
-	global last_update, stats
+	global last_update, stats, last_val
 	
 	cur_time = time.time()
 
@@ -105,34 +266,53 @@ def update_stats():
 	else:
 		last_update = cur_time
 
-	#####
-	# Update memory utilization
-	try:
-		i=0
-		for proc,regex in PROCESSES.items():
-			logging.debug(' updating cpu and mem usage for ' + proc)
+	for proc,val in PROCESSES.items():
+		logging.debug(' updating for ' + proc)
 
-			awk_cmd = "ps -Ao pcpu,rss,pid,user,args | awk '/" + regex + "/ && !/awk/ && !/procstat\.py/ {_cpu+=$1; _rss+=$2} END {printf(\"%.1f %d\", _cpu,_rss)}'"
-			p = subprocess.Popen(awk_cmd, shell=True, stdout=subprocess.PIPE)
-			out, err = p.communicate()
-			logging.debug('  result: ' + out)
+		# setup storage lists
+		if not proc in stats:
+			stats[proc] = {}
+		if not proc in last_val:
+			last_val[proc] = {}
 
-			if p.returncode:
-				logging.warning('failed getting stats')
-				continue
+		#####
+		# Update CPU utilization
+		try:
+			(ppid, pgid) = get_pgid(proc)
+		except Exception, e:
+			logging.warning(' failed getting pgid: ' + str(e))
+			continue
 
-			vars = out.split()
-			logging.debug('  vars: ' + str(vars))
+		# save for later
+		pgid_list[proc] = (ppid, pgid)
 
-			stats[proc + '_cpu'] = float(vars[0])
-			stats[proc + '_mem'] = int(vars[1])
+		pids = get_pgroup(ppid, pgid)
 
+		cpu_time = time.time()
+		proc_time = 0
+		for p in pids:
+			proc_time += readCpu(p)
+		
+		logging.debug(' proc_time: ' + str(proc_time) + ' cpu_time: ' + str(cpu_time))
 
-	except:
-		logging.warning('error refreshing stats')
-		logging.warning(traceback.format_exc())
-		return False
+		# do we have an old value to calculate with?
+		if 'cpu_time' in last_val[proc]:
+			logging.debug(' last_val: ' + str(last_val[proc]))
+			logging.debug(' calc: 100 * ' + str(proc_time - last_val[proc]['proc_time']) + ' / ' + str(cpu_time - last_val[proc]['cpu_time']) + ' * ' + str(JIFFIES_PER_SEC))
+			stats[proc]['cpu'] = 100 * (proc_time - last_val[proc]['proc_time']) / float((cpu_time - last_val[proc]['cpu_time']) * JIFFIES_PER_SEC)
 
+			logging.debug(' calc: ' + str(stats[proc]['cpu']))
+		else:
+			stats[proc]['cpu'] = 0.0
+
+		last_val[proc]['cpu_time'] = cpu_time
+		last_val[proc]['proc_time'] = proc_time
+
+		#####
+		# Update Mem utilization	
+		rss = get_rss(pids)
+		stats[proc]['mem'] = rss
+	
 	logging.debug('success refreshing stats')
 	logging.debug('stats: ' + str(stats))
 
@@ -145,7 +325,17 @@ def get_stat(name):
 
 	if ret:
 		if name.startswith('procstat_'):
-			label = name[9:]
+			fir = name.find('_')
+			sec = name.find('_', fir + 1)
+
+			proc = name[fir+1:sec]
+			label = name[sec+1:]
+
+			try:
+				return stats[proc][label]
+			except:
+				logging.warning('failed to fetch [' + proc + '] ' + name)
+				return 0
 		else:
 			label = name
 
@@ -164,6 +354,8 @@ def metric_init(params):
 	logging.debug('init: ' + str(params))
 
 	PROCESSES = params
+
+	#for proc,regex in PROCESSES.items():
 		
 	update_stats()
 
@@ -183,7 +375,7 @@ def metric_init(params):
 	descriptors = []
 	for label in descriptions:
 		for proc in PROCESSES:
-			if stats.has_key(proc + '_' + label):
+			if stats[proc].has_key(label):
 
 				d = {
 					'name': 'procstat_' + proc + '_' + label,
@@ -203,11 +395,53 @@ def metric_init(params):
 				descriptors.append(d)
 
 			else:
-				logging.warning("skipped " + proc + '_' + label)
+				logging.error("skipped " + proc + '_' + label)
 
 	#logging.debug('descriptors: ' + str(descriptors))
 
 	return descriptors
+
+def display_proc_stat(pid):
+	try:
+		stat = file('/proc/' + pid + '/stat', 'rt').readline().split()
+
+		fields = [
+			'pid', 'comm', 'state', 'ppid', 'pgrp', 'session',
+			'tty_nr', 'tty_pgrp', 'flags', 'min_flt', 'cmin_flt', 'maj_flt',
+			'cmaj_flt', 'utime', 'stime', 'cutime', 'cstime', 'priority',
+			'nice', 'num_threads', 'it_real_value', 'start_time', 'vsize', 'rss',
+			'rlim', 'start_code', 'end_code', 'start_stack', 'esp', 'eip',
+			'pending', 'blocked', 'sigign', 'sigcatch', 'wchan', 'nswap',
+			'cnswap', 'exit_signal', 'processor', 'rt_priority', 'policy'
+		]
+
+		# Display them
+		i = 0
+		for f in fields:
+			print '%15s: %s' % (f, stat[i])
+			i += 1
+
+	except:
+		print('failed to get /proc/' + pid + '/stat')
+		print(traceback.print_exc(file=sys.stdout))
+
+def display_proc_statm(pid):
+	try:
+		statm = file('/proc/' + pid + '/statm', 'rt').readline().split()
+
+		fields = [
+			'size', 'rss', 'share', 'trs', 'drs', 'lrs' ,'dt' 
+		]
+
+		# Display them
+		i = 0
+		for f in fields:
+			print '%15s: %s' % (f, statm[i])
+			i += 1
+
+	except:
+		print('failed to get /proc/' + pid + '/statm')
+		print(traceback.print_exc(file=sys.stdout))
 
 def metric_cleanup():
 	logging.shutdown()
@@ -220,7 +454,9 @@ if __name__ == '__main__':
 	logging.debug('running from cmd line')
 	parser = OptionParser()
 	parser.add_option('-p', '--processes', dest='processes', default='', help='processes to explicitly check')
-	parser.add_option('-r', '--regex', dest='regex', default='', help='regex for each processes')
+	parser.add_option('-v', '--value', dest='value', default='', help='regex or pidfile for each processes')
+	parser.add_option('-s', '--stat', dest='stat', default='', help='display the /proc/[pid]/stat file for this pid')
+	parser.add_option('-m', '--statm', dest='statm', default='', help='display the /proc/[pid]/statm file for this pid')
 	parser.add_option('-b', '--gmetric-bin', dest='gmetric_bin', default='/usr/bin/gmetric', help='path to gmetric binary')
 	parser.add_option('-c', '--gmond-conf', dest='gmond_conf', default='/etc/ganglia/gmond.conf', help='path to gmond.conf')
 	parser.add_option('-g', '--gmetric', dest='gmetric', action='store_true', default=False, help='submit via gmetric')
@@ -229,27 +465,39 @@ if __name__ == '__main__':
 
 	(options, args) = parser.parse_args()
 
+	if options.stat != '':
+		display_proc_stat(options.stat)
+		sys.exit(0)
+	elif options.statm != '':
+		display_proc_statm(options.statm)
+		sys.exit(0)
+
 	_procs = options.processes.split(',')
-	_regex = options.regex.split(',')
+	_val = options.value.split(',')
 	params = {}
 	i = 0
 	for proc in _procs:
-		params[proc] = _regex[i]
+		params[proc] = _val[i]
 		i += 1
 	
 	if options.test:
 		test(params)
-		sys.exit(0)
+		update_stats()
+
+		print('')
+		print(' waiting ' + str(MAX_UPDATE_TIME) + ' seconds')
+		time.sleep(MAX_UPDATE_TIME)
 
 	metric_init(params)
 
 	for d in descriptors:
 		v = d['call_back'](d['name'])
 		if not options.quiet:
-			print ' %s: %s %s [%s]' % (d['name'], v, d['units'], d['description'])
+			print ' %s: %s %s [%s]' % (d['name'], d['format'] % v, d['units'], d['description'])
 
 		if options.gmetric:
 			cmd = "%s --conf=%s --value='%s' --units='%s' --type='%s' --name='%s' --slope='%s'" % \
 				(options.gmetric_bin, option.gmond_conf, v, d['units'], d['value_type'], d['name'], d['slope'])
 			os.system(cmd)
+
 
