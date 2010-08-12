3dee42d09194b62ecd2509de0caa184d6b487ecb
pdt256 <pdt256@45b1daad-ef76-c2da-c1da-ee31385fce6c>
2010-08-12 20:53:58 +0000

    Adding jetty sessions label.

--- trunk/graph.d/jmx_sessions_report.php
+++ trunk/graph.d/jmx_sessions_report.php
@@ -24,10 +24,11 @@ function graph_jmx_sessions_report ( &$rrdtool_graph ) {
     }
     $rrdtool_graph['lower-limit']    = '0';
     $rrdtool_graph['height'] += ($size == 'medium') ? 28 : 0;
+	$rrdtool_graph['vertical-label'] = 'sessions';
 
 	$series = "DEF:'sessions'='${rrd_dir}/jmx_${jmx}_sessions.rrd':'sum':AVERAGE "
 		."AREA:'sessions'#ADCFF5:'' "
-		."LINE2:'sessions'#000098:'Sessions' "
+		."LINE2:'sessions'#000098:'${jmx}' "
 	;
 
     $rrdtool_graph['series'] = $series;
