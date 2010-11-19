7bbec2116114efabb5e33565c1561aa3affaa779
pdt256 <pdt256@45b1daad-ef76-c2da-c1da-ee31385fce6c>
2010-11-19 10:43:55 +0000

    Moving rainbow colors to fix dependency.

--- trunk/graph.d/procstat_cpu_report.php
+++ trunk/graph.d/procstat_cpu_report.php
@@ -6,11 +6,25 @@ function graph_procstat_cpu_report ( &$rrdtool_graph ) {
     global $context,
            $hostname,
            $graph_var,
-           $rainbow,
            $range,
            $rrd_dir,
            $size,
            $strip_domainname;
+
+$rainbow = array(
+    '008000', // green
+    '0000FF', // blue
+    'FF0000', // red
+    'FFEA00', // yellow
+    '9900CC', // purple
+    '00FF00', // lime green
+    '00FFFF', // aqua blue
+    'FF00FF', // magenta
+    '808000', // olive
+    '008080', // teal
+    '808080', // gray
+    '000000'  // black
+);
 
     if ($strip_domainname) {
        $hostname = strip_domainname($hostname);
@@ -33,6 +47,7 @@ function graph_procstat_cpu_report ( &$rrdtool_graph ) {
 
 		$series .= "DEF:'${proc}'='${rrd_dir}/procstat_${proc}_cpu.rrd':'sum':AVERAGE "
 		."LINE2:'${proc}'#${color}:'${proc}' ";
+		//."LINE2:'${proc}'#555555:'${proc}' ";
 	}
 
     $rrdtool_graph['series'] = $series;
